6de517b4c1450968bf7acbaf9c73907dcb8f9a2a
Fuat Güzel <fuatguzeel@gmail.com>
2021-08-03 20:24:09 +0300

    Update MyComponent and branch name

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const MyComponent = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.age),
-  ]);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.age}</h3>
+    </div>
+  );
 };
 
 export default MyComponent;
