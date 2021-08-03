1aaa96e82c3130daaf62eae0d00d280491a0251f
Fuat Güzel <fuatguzeel@gmail.com>
2021-08-03 21:29:30 +0300

    Hooks example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MyComponent from "./MyComponent";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Hi React!</h1>
+      <SearchParams />
       <MyComponent name="Ali" age="24" />
       <MyComponent name="Ayşe" age="12" />
     </div>
