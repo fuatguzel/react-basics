2d72ab90c8f13d3a0c8f887f4e5d5950693087d7
Fuat Güzel <fuatguzeel@gmail.com>
2021-08-04 23:44:09 +0300

    Add some custom styles

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MyComponent = (props) => {
   return (
-    <div>
+    <div className="my-styles">
       <h2>{props.name}</h2>
       <h3>{props.age}</h3>
     </div>
