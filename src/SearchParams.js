96c1bf60a6de7e29bcb84e2e6b790514f8e4cdf9
Fuat Güzel <fuatguzeel@gmail.com>
2021-08-04 23:26:49 +0300

    Add Pet.js and Call api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,60 +1,83 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
-const JOBS = ["Doctor", "Engineer", "Nurse", "Pilot", "Singer"];
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("0");
-  const [jobs, setJobs] = useState("");
+  const [animal, updateAnimal] = useState("");
+  const [location, updateLocation] = useState("");
+  const [breed, updateBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const breeds = [];
 
-  function updateName(e) {
-    setName(e.target.value);
-  }
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  function updateAge(e) {
-    setAge(e.target.value);
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
   }
 
   return (
     <div className="search-params">
       <form>
-        <label htmlFor="name">
-          Name
+        <label htmlFor="location">
+          Location
           <input
-            id="name"
-            onChange={updateName}
-            value={name}
-            placeholder="Name"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => updateLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="age">
-          Age
-          <input
-            type="number"
-            id="age"
-            onChange={updateAge}
-            value={age}
-            placeholder="Age"
-          />
-        </label>
-        <label htmlFor="job">
-          Jobs
+        <label htmlFor="animal">
+          Animal
           <select
-            id="jobs"
-            onChange={(e) => setJobs(e.target.value)}
-            onBlur={(e) => setJobs(e.target.value)}
-            value={jobs}
+            id="animal"
+            value={animal}
+            onChange={(e) => updateAnimal(e.target.value)}
+            onBlur={(e) => updateAnimal(e.target.value)}
           >
             <option />
-            {JOBS.map((job) => (
-              <option value={job} key={job}>
-                {job}
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => updateBreed(e.target.value)}
+            onBlur={(e) => updateBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
               </option>
             ))}
           </select>
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
